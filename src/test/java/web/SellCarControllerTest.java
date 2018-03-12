cdf6e5837e6444073bac48da45760756346692cc
Maciek Boczkowski <boczkowski.maciej@gmail.com>
2018-03-12 09:57:55 +0100

    Test displayAllCars method 1

--- src/test/java/web/SellCarControllerTest.java
+++ src/test/java/web/SellCarControllerTest.java
@@ -1,41 +1,34 @@
 package web;
 
-import config.Config;
-import entity.CarEntity;
-import org.assertj.core.api.SoftAssertions;
+import model.Car;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import repository.CarRepository;
 import service.CarService;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {Config.class})
-@WebAppConfiguration
+@RunWith(MockitoJUnitRunner.class)
+//@ContextConfiguration(classes = {Config.class})
+//@WebAppConfiguration
 public class SellCarControllerTest {
 
-    @Autowired
-    private CarRepository carRepository;
-    private CarService carService = new CarService(carRepository);
-    private SellCarController controller = new SellCarController(carService);
+//    @Mock
+//    private CarService carService;
+
+    CarService carService = mock(CarService.class);
     private MockMvc mockMvc;
 
 
@@ -44,17 +37,26 @@ public class SellCarControllerTest {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/view/");
         viewResolver.setSuffix(".jsp");
+
+        SellCarController controller = new SellCarController(carService);
         mockMvc = standaloneSetup(controller).setViewResolvers(viewResolver).build();
     }
 
 
     @Test
     public void displayAllCars() throws Exception {
-        MvcResult result = mockMvc.perform(get("/display")).andExpect(status().isOk()).andReturn();
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(result.getModelAndView().getViewName()).isEqualTo("car");
-        softly.assertThat(carService.displayAllCars()).hasSize(0);
-        softly.assertAll();
+        given(carService.displayAllCars()).willReturn(Collections.emptyList());
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Ferrari", "F40", 1990, "www.ferrari.pl", 256000));
+//        carList.add(new Car("Porsche","Macan",2016,"www.porsche.pl",199000));
+//        carList.add(new Car("Maseratti","Quatroporte",2010,"www.maseratti.pl",65500));
+
+        given(carService.displayAllCars()).willReturn(carList);
+
+//        mockMvc.perform(get("/display")).andExpect(status().isOk());
+        mockMvc.perform(get("/display")).andExpect(status().isOk()).andExpect(content()
+                .json("[{brand:'Ferrari',model:'F40',year:1990,link:'www.ferrari.pl',price:256000}]"));
     }
 
     @Test
@@ -63,6 +65,7 @@ public class SellCarControllerTest {
 
     @Test
     public void displayAllCarsFilter() throws Exception {
+
     }
 
 }
